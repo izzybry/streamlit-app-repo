6d73e4731f35e0cc5309d6f3b3f1cb93288306c6
Isabel Bryant <ibryant@wellesley.edu>
2022-11-13 17:25:45 -0500

    Added language/app multi-select

--- streamlitApp.py
+++ streamlitApp.py
@@ -33,196 +33,75 @@ st.title('Curious Learning')
 st.header('User Acquisition and Engagement Metrics')
 
 # Set up sidebar with date picker
-add_date_range = st.sidebar.date_input(
+select_date_range = st.sidebar.date_input(
     "Select Date Range:",
     ((pd.to_datetime("today").date() - pd.Timedelta(28, unit='D')),
         pd.to_datetime("today").date()),
     key = 'date_range'
 )
 
-# Set sql query, parameters, and job config
-# sql_query = """
-#     SELECT event_date, event_name, properties, traffic_source.source, traffic_source.name, geo.country, geo.city
-#     FROM `ftm-hindi.analytics_174638281.events_20*`,
-#     UNNEST (user_properties) as properties
-#     WHERE parse_date('%y%m%d', _table_suffix) between @start and @end
-#     and event_name = 'first_open'
-# """
-sql_query = """
-    SELECT event_date, event_name, properties, traffic_source.source, traffic_source.name, geo.country, geo.city
-    FROM `ftm-english.analytics_152408808.events_20*`,
-    UNNEST (user_properties) as properties
-    WHERE parse_date('%y%m%d', _table_suffix) between @start and @end
-    and event_name = 'first_open'
-"""
-
-query_parameters = [
-    bigquery.ScalarQueryParameter("start", "DATE", st.session_state['date_range'][0]),
-    bigquery.ScalarQueryParameter("end", "DATE", st.session_state['date_range'][1])
-]
-job_config = bigquery.QueryJobConfig(
-    query_parameters = query_parameters
+select_language = st.sidebar.multiselect(
+    "Select Language / App",
+    ["FTM - English", "FTM - Afrikaans"],
+    ["FTM - English", "FTM - Afrikaans"],
+    key = 'language_app'
 )
 
-# Reference: https://docs.streamlit.io/knowledge-base/tutorials/databases/bigquery
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-#@st.experimental_memo(ttl=600)
-def run_query(query, _config):
-    rows_raw = client.query(query, job_config = _config)
-    # Convert to list of dicts. Required for st.experimental_memo to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
-    
-rows = run_query(sql_query, job_config)
+app_propertyID_dict = {
+    "FTM - English": "152408808",
+    "FTM - Afrikaans": "177200876"
+}
 
-# st.write(sql_query)
-
-df = pd.DataFrame(rows)
-df = df.sort_values(by=['event_date'])
-
-# st.write("raw df len = " + str(df.shape[0]))
-# st.write(df)
-
-# Convert event_date column to datetime
-df['event_date'] = pd.to_datetime(df['event_date'])
-df['event_date'] = df['event_date'].dt.date
-
-st.write("df len = " + str(df.shape[0]))
-st.write(df)
-
-# user acquisition metrics
-st.subheader('User Acquisition')
-
-usersByDay = df[df['event_name'] == 'first_open'].groupby(
-    ['event_date'])['event_date'].count().reset_index(name='count')
-
-#usersByDay
-
-usersByDayFig = px.line(usersByDay, x='event_date', y='count', labels={
-                     "event_date": "Date (Day)",
-                     "count": "New Users"},
-                     title = "New User Count by Day")
-st.plotly_chart(usersByDayFig)
-
-usersByWeek = usersByDay
-usersByWeek['event_date'] = pd.to_datetime(usersByWeek['event_date']) - pd.to_timedelta(6, unit='d')
-usersByWeek = usersByDay.groupby(
-    [pd.Grouper(key='event_date', freq='W')])['count'].sum().reset_index()
-
-#st.write(usersByWeek)
-
-usersByWeekFig = px.line(usersByWeek, x='event_date', y='count', labels={
-                     'event_date': "Week Starting Date",
-                     'count': "New Users"},
-                     title = "New User Count by Week")
-st.plotly_chart(usersByWeekFig)
-
-df_source_count = df.groupby(["source"])["source"].count().reset_index(name="count")
-fig = px.pie(df_source_count, values='count', names='source')
-fig.update_layout({
-    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-    'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-st.plotly_chart(fig)
-
-df_country_count = df.groupby(["country"])["country"].count().reset_index(name="count")
-
-st.write("df_country_count len = " + str({df_country_count.shape[0]}))
-st.write(df_country_count)
-
-# , locationmode=‘country names’)
-fig = px.choropleth(df_country_count, locations='country', color='count',
-                    locationmode="country names")
-fig.update_layout({
-    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-    'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-st.plotly_chart(fig)
-
-# df = pd.concat([df_raw, df_traffic_source], axis=1)
-# st.write(f"df len = {df.size}")
-# st.write(df)
-
-
-# for r in df.iterrows():
-#     st.write(r)
-
-st.markdown("""---""")
-st.subheader('Google Analytics Data')
-
-# GOOGLE ANALYTICS DATA API SET UP
-# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = st.secrets
-# ga_client = BetaAnalyticsDataClient(credentials=ga_credentials)
-
-afrikaans_property_id = '177200876'
-english_property_id = '152408808'
-
-# creating GA4Report object instances
+# get Google Analytics credentials from secrets
 ga_credentials = service_account.Credentials.from_service_account_info(
     st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
 )
-afrikaans_ga4 = ga4.GA4Report(afrikaans_property_id, ga_credentials)
-english_ga4 = ga4.GA4Report(english_property_id, ga_credentials)
-
-# variables
+# set global report variables
 dimension_list = ['date', 'country']
 metric_list = ['active1DayUsers', 'active7DayUsers', 'active28DayUsers',
                 'newUsers']
 date_range = (st.session_state['date_range'][0].strftime('%Y-%m-%d'),
                 st.session_state['date_range'][1].strftime('%Y-%m-%d'))
 
-# run the GA4 report
-afrikaans_report_df = afrikaans_ga4.run_report(
-    dimension_list, metric_list,
-    date_ranges = [date_range],
-    #row_limit = 1000,
-    offset_row = 0
-)
-english_report_df = english_ga4.run_report(
-    dimension_list, metric_list,
-    date_ranges = [date_range],
-    #row_limit = 1000,
-    offset_row = 0
-)
-
-afrikaans_report_df = pd.DataFrame(columns = afrikaans_report_df['headers'],
-                                    data = afrikaans_report_df['rows'])
-english_report_df = pd.DataFrame(columns = english_report_df['headers'],
-                                    data = english_report_df['rows'])
+# get data for all selected language / app buckets
+main_df = pd.DataFrame()
+for i in st.session_state['language_app']:
+    property_id = app_propertyID_dict[i]
+    ga4_report = ga4.GA4Report(property_id, ga_credentials)
+    report_df = ga4_report.run_report(
+        dimension_list, metric_list,
+        date_ranges = [date_range],
+        #row_limit = 1000,
+        offset_row = 0
+    )
+    report_df = pd.DataFrame(columns = report_df['headers'],
+                                    data = report_df['rows'])
+    report_df.insert(1, "Language/App Bucket", i)
+    main_df = pd.concat([main_df, report_df])
 
 # Convert date column to datetime, then sort by date
-afrikaans_report_df['date'] = pd.to_datetime(afrikaans_report_df['date'])
-afrikaans_report_df['date'] = afrikaans_report_df['date'].dt.date
-afrikaans_report_df = afrikaans_report_df.sort_values(by=['date'])
-english_report_df['date'] = pd.to_datetime(english_report_df['date'])
-english_report_df['date'] = english_report_df['date'].dt.date
-english_report_df = english_report_df.sort_values(by=['date'])
+main_df['date'] = pd.to_datetime(main_df['date'])
+main_df['date'] = main_df['date'].dt.date
+main_df = main_df.sort_values(by=['date'])
+st.write(main_df)
 
 # --- Active Users by Day ---
-af_active_users_df = afrikaans_report_df[['date', 'active1DayUsers', 'active7DayUsers', 'active28DayUsers']]
-en_active_users_df = english_report_df[['date', 'active1DayUsers', 'active7DayUsers', 'active28DayUsers']]
+active_users_df = main_df[
+    ['date', 'active1DayUsers', 'active7DayUsers', 'active28DayUsers']]
 
-af_active_users_df = af_active_users_df.astype({
-    'active1DayUsers': 'int32',
-    'active7DayUsers': 'int32',
-    'active28DayUsers': 'int32'})
-en_active_users_df = en_active_users_df.astype({
+active_users_df = active_users_df.astype({
     'active1DayUsers': 'int32',
     'active7DayUsers': 'int32',
     'active28DayUsers': 'int32'})
 
 # Group data by day and plot as timeseries chart
-af_active_users_df = af_active_users_df.groupby(['date']).sum().reset_index()
-st.write(af_active_users_df)
-en_active_users_df = en_active_users_df.groupby(['date']).sum().reset_index()
-st.write(en_active_users_df)
+active_users_df = active_users_df.groupby(['date']).sum().reset_index()
+st.write(active_users_df)
 
-af_active_users_fig = px.line(af_active_users_df, x='date',
+active_users_fig = px.line(active_users_df, x='date',
                             y=['active1DayUsers', 'active7DayUsers', 'active28DayUsers'],
-                                title = "Afrikaans - Active Users by Day",
+                                title = "Active Users by Day",
                                 labels={"value": "Active Users", "date": "Date", "variable": "Trailing"})
-st.plotly_chart(af_active_users_fig)
-en_active_users_fig = px.line(en_active_users_df, x='date',
-                            y=['active1DayUsers', 'active7DayUsers', 'active28DayUsers'],
-                                title = "US English - Active Users by Day",
-                                labels={"value": "Active Users", "date": "Date", "variable": "Trailing"})
-st.plotly_chart(en_active_users_fig)
+st.plotly_chart(active_users_fig)
+
   # -----------------------------------------
