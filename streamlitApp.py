6e141cf7d98bec06282dbc36b85e2a8a1ce5b2d8
Isabel Bryant <ibryant@wellesley.edu>
2022-12-22 12:20:54 -0500

    Adding 2 vizs

--- streamlitApp.py
+++ streamlitApp.py
@@ -12,6 +12,7 @@ import db_dtypes
 import json
 import plotly
 import plotly.express as px
+import plotly.graph_objects as go
 
 # --- GLOBAL VARIABLES ---
 countries_df = pd.read_csv('countries.csv')
@@ -74,8 +75,46 @@ def get_campaign_metrics():
                             data = camp_metrics_rows)
     return camp_metrics_data
 
+def get_ra_segments(campaign_data, app_data, user_data):
+    df = pd.DataFrame(columns = ['segment', 'la', 'perc_la', 'ra', 'avg_learner_ra', 'rac'])
+    total_lvls = app_data['total_lvls'][0]
+    seg = []
+    ra = []
+    for lvl in user_data['max_lvl']:
+        perc = lvl / total_lvls
+        ra.append(perc)
+        l = [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
+        if perc < .1:
+            seg.append(.1)
+        elif .1 <= perc < .2:
+            seg.append(.2)
+        elif .2 <= perc < .3:
+            seg.append(.3)
+        elif .3 <= perc < .4:
+            seg.append(.4)
+        elif .4 <= perc < .5:
+            seg.append(.5)
+        elif .5 <= perc < .6:
+            seg.append(.6)
+        elif .6 <= perc < .7:
+            seg.append(.7)
+        elif .7 <= perc < .8:
+            seg.append(.8)
+        elif .8 <= perc < .9:
+            seg.append(.9)
+        else:
+            seg.append(1)
+    user_data['seg'] = seg
+    user_data['ra'] = ra
+    res = user_data.groupby('seg').agg(la=('user_pseudo_id','count'), avg_learner_ra=('ra','mean')).reset_index()
+    res['la_perc'] = res['la'] / res['la'].sum()
+    res['ra'] = res['la'] * res['avg_learner_ra']
+    res['rac'] = campaign_data['Total Cost (USD)'][0] * res['la_perc'] / res['ra']
+    return res
+
+
 # --- UI ---
-st.title('Curious Learning')
+st.title('CL Feed the Monster Metrics Dashboard')
 expander = st.expander('Definitions')
 expander.write('Learner Acquisition (LA) = number of users that have successfully completed at least one FTM level')
 expander.write('Learner Acquisition Date = the date a user completed level one of their FTM app')
@@ -107,7 +146,6 @@ for campaign in st.session_state['campaigns']:
         temp = ftm_users[(ftm_users['LA_date'] >= start_date) & (ftm_users['LA_date'] <= end_date) & (ftm_users['app_id'] == app) & (ftm_users['country'] == country)]
     temp['campaign'] = campaign
     users_df = pd.concat([users_df, temp])
-
 st.write('Total Learners Acquired During Campaign(s): ', str(len(users_df)))
 
 daily_la = users_df.groupby(['campaign', 'LA_date'])['user_pseudo_id'].count().reset_index(name='Learners Acquired')
@@ -143,7 +181,7 @@ st.plotly_chart(daily_la_fig)
 monthly_la = users_df
 monthly_la['LA_YM'] = (pd.to_datetime(monthly_la.LA_date)).dt.strftime('%Y-%m')
 monthly_la = users_df.groupby(['campaign', 'LA_YM'])['user_pseudo_id'].count().reset_index(name='Learners Acquired')
-monthly_la_fig = px.area(monthly_la,
+monthly_la_fig = px.bar(monthly_la,
     x='LA_YM',
     y='Learners Acquired',
     color='campaign',
@@ -163,11 +201,34 @@ country_fig = px.choropleth(country_la,
 country_fig.update_layout(geo=dict(bgcolor= 'rgba(0,0,0,0)'))
 st.plotly_chart(country_fig)
 
-# LEARNER ACQUISITION COST
+# READING ACQUISITION SEGMENTATION
+if len(st.session_state['campaigns']) == 1:
+    ra_segs = get_ra_segments(ftm_campaigns, ftm_apps, users_df)
+    ra_segs_fig = px.bar(ra_segs,
+        x='seg',
+        y='la',
+        labels={
+            'la': 'Learners',
+            'seg': 'Percent Reading Acquired'
+        },
+        title='Learners by Percentage of Reading Acquired ' 
+    )
+    st.plotly_chart(ra_segs_fig)
+    ra_segs = ra_segs[['seg', 'la', 'la_perc', 'avg_learner_ra', 'ra', 'rac']]
+    ra_segs = ra_segs.rename(columns={
+        'seg': 'Decile',
+        'la': 'Learners',
+        'la_perc': '%% Learners',
+        'avg_learner_ra': 'Average RA / Learner',
+        'ra': 'RA',
+        'rac': 'RAC (USD)'})
+    st.write(ra_segs)
+
+
+# LEARNER & READING ACQUISITION COST
 ftm_campaign_metrics = get_campaign_metrics()
 ftm_campaign_metrics = ftm_campaign_metrics[ftm_campaign_metrics['campaign_name'].isin(st.session_state['campaigns'])]
 ftm_campaign_metrics['camp_age'] = [(pd.to_datetime("today").date() - ftm_campaigns.loc[ftm_campaigns['Campaign Name'] == c, 'Start Date'].item()).days for c in ftm_campaign_metrics['campaign_name']]
-# st.write('ftm_campaign_metrics', ftm_campaign_metrics)  
 lavslac = px.scatter(ftm_campaign_metrics,
     x='la',
     y='lac',
