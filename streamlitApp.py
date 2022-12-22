8fabd72dc7f2cf3f0d06e74e151689ba60bc0e7d
Isabel Bryant <ibryant@wellesley.edu>
2022-12-22 13:46:42 -0500

    Adding st.cache

--- streamlitApp.py
+++ streamlitApp.py
@@ -37,6 +37,7 @@ def run_query(query):
     rows = rows.fetchall()
     return rows
 
+@st.cache
 def get_campaign_data():
     campaign_sheet_url = st.secrets["Campaign_gsheets_url"]
     campaign_rows = run_query(f'SELECT * FROM "{campaign_sheet_url}"')
@@ -50,6 +51,7 @@ def get_campaign_data():
     })
     return campaign_data
 
+@st.cache
 def get_user_data():
     sql_query = f"""
         SELECT * FROM `dataexploration-193817.user_data.ftm_users`
@@ -61,6 +63,7 @@ def get_user_data():
     df['max_lvl_date'] = (pd.to_datetime(df['max_lvl_date'])).dt.date
     return df
 
+@st.cache
 def get_apps_data():
     apps_sheet_url = st.secrets["ftm_apps_gsheets_url"]
     apps_rows = run_query(f'SELECT app_id, language, bq_property_id, bq_project_id, total_lvls FROM "{apps_sheet_url}"')
@@ -68,6 +71,7 @@ def get_apps_data():
         data = apps_rows)
     return apps_data
 
+@st.cache
 def get_campaign_metrics():
     camp_metrics_url = st.secrets["campaign_metrics_gsheets_url"]
     camp_metrics_rows = run_query(f'SELECT * FROM "{camp_metrics_url}"')
@@ -75,6 +79,7 @@ def get_campaign_metrics():
                             data = camp_metrics_rows)
     return camp_metrics_data
 
+@st.cache
 def get_ra_segments(campaign_data, app_data, user_data):
     df = pd.DataFrame(columns = ['segment', 'la', 'perc_la', 'ra', 'avg_learner_ra', 'rac'])
     total_lvls = app_data['total_lvls'][0]
